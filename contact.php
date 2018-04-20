a3c9607db048e93f85c8ad87e4224c4e77b9d42b
Rachael97 <kaijing0319@gmail.com>
2018-04-20 21:04:32 +0800

    add contact page

--- contact.php
+++ contact.php
@@ -5,17 +5,18 @@
 	</head>
 	
 	<body>
-		
+	
 		<ul>
 			<li><a href="index.php">Home</a></li>
 			<li><a href="about.php">About</a></li>
 			<li><a href="contact.php">Contact</a></li>
 		</ul>
 		
-		<h1>My Home (Sweet Shop)</h1>
-
-		<p>Welcome to our website!!!</p>
-
+		<h1>Contact Us</h1>
+		
+		Name: <input type="text" placeholder="Enter your name"/>
+		Mobile: <input type="text" placeholder="Enter your mobile"/>
+		
 	</body>
 	
 </html>
